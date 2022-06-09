a6fbae56ba37ae23061ab1b113247d765ed07973
GhislainVentre <magistis@magis>
2022-06-09 16:33:24 +0200

    Initial code

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+//include SDL2
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
